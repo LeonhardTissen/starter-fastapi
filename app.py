550561f072875496afb8e544ac7b216d7d0aae48
Leonhard Tissen <contact@warze.org>
2024-01-02 11:17:17 +0100

    Add CORS middleware to allow any origin

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
 import json
 
 app = FastAPI()
@@ -7,17 +8,25 @@ app = FastAPI()
 with open('words_alpha.json') as f:
 	words = json.load(f)
 
+# Add CORS middleware to allow any origin
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=['*'],
+	allow_methods=['*'],
+	allow_headers=['*'],
+)
+
 @app.get('/ww/check')
 async def check_word(word: str = Query(...)):
-    if word in words:
-        return {'result': True}
-    else:
-        return {'result': False}
+	if word in words:
+		return {'result': True}
+	else:
+		return {'result': False}
 
 @app.get('/ww/match')
 async def get_matching_word(word: str = Query(...), length: int = Query(0, gt=0), reverse: bool = Query(False)):
-    for w in words:
-        if len(w) == length and (w.startswith(word) or (reverse and w.endswith(word))):
-            return {'matching_word': w}
-    
-    return {'message': 'No matching word found'}
+	for w in words:
+		if len(w) == length and (w.startswith(word) or (reverse and w.endswith(word))):
+			return {'matching_word': w}
+	
+	return {'message': 'No matching word found'}
