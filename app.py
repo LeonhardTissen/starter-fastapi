7e5b3c18478e4282d71da74437741a1c2ca51ba6
Leonhard Tissen <contact@warze.org>
2024-01-02 11:03:51 +0100

    Remove unused imports and code

--- app.py
+++ app.py
@@ -1,32 +1,20 @@
 from fastapi import FastAPI, Query
-from fastapi.responses import FileResponse
 import json
 
-from pydantic import BaseModel
-
 app = FastAPI()
-
-
-class Item(BaseModel):
-    item_id: int
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
 # Load the JSON file containing all words
 with open('words_alpha.json') as f:
 	words = json.load(f)
 
-@app.get('/checkword')
+@app.get('/ww/check')
 async def check_word(word: str = Query(...)):
     if word in words:
         return {'result': True}
     else:
         return {'result': False}
 
-@app.get('/getmatchingword')
+@app.get('/ww/match')
 async def get_matching_word(word: str = Query(...), length: int = Query(0, gt=0), reverse: bool = Query(False)):
     for w in words:
         if len(w) == length and (w.startswith(word) or (reverse and w.endswith(word))):
